4347602be4719bda754422c334f4e9b767272481
diana esteves <diana@pulumi.com>
2024-02-20 10:06:05 -0600

    updated deps

--- infra/aws-oidc/index.ts
+++ infra/aws-oidc/index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as pulumiservice from "@pulumi/pulumiservice";
 import * as aws from "@pulumi/aws";
 import { stringify } from 'yaml'
-import createESCEnvironment from './preview-api-esc';
+import upsertEnvironment from './preview-api-esc';
 
 // Configurations
 const audience = pulumi.getOrganization();
@@ -11,13 +11,32 @@ const oidcIdpUrl: string = config.require('oidcIdpUrl');
 const thumbprint: string = config.require('thumbprint');
 export const escEnv: string = config.require('escEnv');
 
-// Create an AWS IAM OIDC Identity Provider.
-// TODO - if the OIDC provider already exists, we should use it instead of creating a new one
+////// Solve ////////////////////////////////////////
+// aws:iam:OpenIdConnectProvider (oidcProvider):
+// error: 1 error occurred:
+//     * creating IAM OIDC Provider: EntityAlreadyExists: Provider with url https://api.pulumi.com/oidc already exists.
+//     status code: 409, request id: 7edf2c50-559a-43cc-a682-f50a40c470bd
+// 1. UNCOMMENT THE BELOW CODE SNIPPET
+// aws.iam.getOpenIdConnectProvider({
+//     url: oidcIdpUrl,
+// }).then(temp => {
+//     console.log("Ensure you imported your existing OIDC Provider");
+//     console.log("pulumi import aws:iam/openIdConnectProvider:OpenIdConnectProvider oidcProvider", temp.arn, "--yes")
+// });
+// 2. RUN `pulumi preview`
+// 3. COPY THE `pulumi import` COMMAND FROM THE CONSOLE AND RUN IT
+// 4. COMMENT THE ABOVE CODE SNIPPET
+// 5. REPLACE THE RESOURCE DEFINITION BELOW TO THAT OF THE CONSOLE
+// 6. RUN `pulumi up`
+////////////////////////////////////////`
 
+// Create a new OIDC Provider
 const oidcProvider = new aws.iam.OpenIdConnectProvider("oidcProvider", {
     clientIdLists: [audience],
     url: oidcIdpUrl, // Replace with your IdP URL
     thumbprintLists: [thumbprint], // Replace with the thumbprint of the IdP server's certificate
+}, {
+    protect: true,
 });
 
 // Create a new role that can be assumed by the OIDC provider
@@ -31,22 +50,28 @@ const role = new aws.iam.Role("oidcProviderRole", {
             Condition: { StringEquals: { [`${url}:aud`]: [audience] } },
         }],
     })),
-});
+}, { dependsOn: [oidcProvider] });
 
 // TODO - attach other policies to the role as needed
-const rolePolicyAttachment = new aws.iam.RolePolicyAttachment("oidcProviderRolePolicyAttachment", {
-    role: role,
-    policyArn: "arn:aws:iam::aws:policy/AdministratorAccess",
-});
+try {
+    new aws.iam.RolePolicyAttachment("oidcProviderRolePolicyAttachment", {
+        role: role,
+        policyArn: "arn:aws:iam::aws:policy/AdministratorAccess",
+    });
+} catch (error) {
+    console.warn("Unable to attach the AdministratorAccess policy to the " + role.name)
+} finally {
+    console.log("Please add/remove policies as necessary.")
+}
 
-// Create a new Pulumi Cloud access token to be used to create the environment
+// Create a new Pulumi Cloud access token to be used to create the Environment
 const accessToken = new pulumiservice.AccessToken("myAccessToken", {
     description: "Used to create an ESC Environment for AWS OIDC",
 }, { dependsOn: [role] });
 
 accessToken.value.apply(tokenId => {
     role.arn.apply(arn => {
-        const yamlStr = stringify(
+        let yamlStr = stringify(
             {
                 "values": {
                     "aws": {
@@ -68,6 +93,8 @@ accessToken.value.apply(tokenId => {
                 },
             }
         );
-        createESCEnvironment(yamlStr, audience, escEnv, tokenId);
+
+        yamlStr = yamlStr + '\n';
+        upsertEnvironment(yamlStr, audience, escEnv, tokenId);
     });
 });
--- infra/aws-oidc/preview-api-esc.ts
+++ infra/aws-oidc/preview-api-esc.ts
@@ -5,33 +5,22 @@ import axios from 'axios';
 // UNTIL PULUMI SUPPORTS THIS FUNCTIONALITY
 // https://www.pulumi.com/docs/pulumi-cloud/cloud-rest-api/#create-environment
 // https://github.com/pulumi/pulumi-pulumiservice/issues/225
-export default function createESCEnvironment(data: string, organization: string, environment: string, token: string): void {
+export default function upsertEnvironment(data: string, organization: string, environment: string, token: string): void {
 
     const headers = {
         'Accept': 'application/vnd.pulumi+8',
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain',
         'Authorization': `token ${token}`,
     };
-
+    // Doing a POST request to create the environment followed by a PATCH because of previews.
     axios.post(`https://api.pulumi.com/api/preview/environments/${organization}/${environment}`, data, { headers })
         .catch(error => {
-            if (error.response) {
-                if (error.response.status === 401) {
-                    console.error('Unauthorized: Please check your Pulumi access token');
-                } else if (error.response.status === 409) {
-                    console.error('ESC Environment already exists');
-                    // console.error('Updating existing ESC Environment');
-                    // axios.patch(`https://api.pulumi.com/api/preview/environments/${organization}/${environment}`, data, { headers })
-                    //     .then(response => {
-                    //         console.log(response.data);
-                    //     })
-                    //     .catch(error => {
-                    //         console.error(error.toString());
-                    //     });
-                } else if (error.response.status === 400) {
-                    console.error('Check your YAML configuration');
-                }
-            }
-            console.error(error.toString());
+          //  console.log("Error creating ESC Environment: " + environment);
+        })
+        .finally(() => {
+            axios.patch(`https://api.pulumi.com/api/preview/environments/${organization}/${environment}`, data, { headers })
+                .then(response => {
+                    console.log("Created ESC Environment: " + environment);
+                });
         });
 }
